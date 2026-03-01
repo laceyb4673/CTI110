65621719768e86606d5fa05807403fdfafa7d6dd
laceyb4673 <laceyb4673@student.faytechcc.edu>
2026-03-01 17:57:43 -0500

    Add files via upload

--- index.py
+++ index.py
@@ -1,4 +1,16 @@
-first_name = input("enter your first name: ")
-last_name = input("enter your last name: ")
-print ("Hello " + first_name + " " + last_name + "!")
- 
+#
+#Name: [Brennan Lacey]
+#Date: [3/1/2026]
+#P1HW1
+#Description: Thia is P1HW1.
+#
+print("#---------Calculating Exponents")
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print(f"{base} raised to the power of {exponent} is: {(base**exponent)} !!")
+
+print("#---Addition and Subrtaction---")
+start_int = int(input("Enter a starting integer: "))
+add_int = int(input("Enter an integer to add: "))
+subtract_int = int(input("Enter an integer to subtract: "))
+print(f"{start_int} + {add_int} - {subtract_int} = {(start_int + add_int - subtract_int)}")
